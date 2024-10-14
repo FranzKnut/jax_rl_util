bf1337e98fc0659509173dd5d8d4cf2e9e1fb823
Julian <julian.lemmel@tuwien.ac.at>
2024-10-14 14:12:05 +0200

    add second_half mask

--- envs/env_util.py
+++ envs/env_util.py
@@ -20,7 +20,7 @@ def render_brax(env, states, render_steps=100, render_start=0, camera=None):
 def make_obs_mask(base_obs_size: int, obs_mask: Iterable[int] | str | int = None):
     """Get the observation mask from string description.
 
-    obs_mask may take values ['odd', 'even', 'first_half'] or a list of indices.
+    obs_mask may take values ['odd', 'even', 'first_half', 'second_half'] or a list of indices.
     """
     # Flat observation size
     if not isinstance(base_obs_size, int):
@@ -30,6 +30,8 @@ def make_obs_mask(base_obs_size: int, obs_mask: Iterable[int] | str | int = None
         obs_mask = [i for i in range(base_obs_size) if i % 2 == (obs_mask == "odd")]
     elif obs_mask == "first_half":
         obs_mask = [i for i in range((base_obs_size + 1) // 2)]
+    elif obs_mask == "second_half":
+        obs_mask = [i for i in range((base_obs_size + 1) // 2, base_obs_size)]
     elif isinstance(obs_mask, int):
         obs_mask = jnp.arange(base_obs_size, dtype=jnp.int32)
     elif obs_mask is None:
