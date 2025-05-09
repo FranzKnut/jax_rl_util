c7644a46f97f2d0e0cb68219f60e9c009a567168
julian <julian.lemmel@tuwien.ac.at>
2025-05-09 16:23:10 +0200

    run_on_gpu context manager

--- jax_rl_util/util/__init__.py
+++ jax_rl_util/util/__init__.py
@@ -1,1 +1,17 @@
 """RL utilities module."""
+
+from contextlib import contextmanager
+
+import jax
+
+
+@contextmanager
+def run_on_gpu():
+    """Context manager to run code on GPU if available."""
+    try:
+        device = jax.devices("gpu")[0]
+    except RuntimeError:
+        print("WARNING: No GPU available, using CPU for training.")
+        device = None
+    with jax.default_device(device):
+        yield
