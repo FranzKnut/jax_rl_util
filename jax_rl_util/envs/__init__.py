0dade5e9e7503ccf357b1c0df9556d041059292a
Julian <julian.lemmel@tuwien.ac.at>
2025-12-29 16:35:18 +0100

    add CarRacingPenalty Env

--- jax_rl_util/envs/__init__.py
+++ jax_rl_util/envs/__init__.py
@@ -2,5 +2,6 @@
 
 from .continuous_cartpole import CartPoleSwingUp, ContinuousCartPoleEnv  # noqa
 from .dronegym import DroneGym  # noqa
+from .carracing_penalty import CarRacingPenaltyEnv  # noqa
 
 BRAX_ENVS_POS_DIMS = {"ant": 2, "halfcheetah": 1, "humanoid": 2}
---
+++ jax_rl_util/envs/carracing_penalty.py
@@ -0,0 +1,24 @@
+"""A version of CarRacing environment with penalty for going off track."""
+
+import gymnasium as gym
+from gymnasium.envs.box2d.car_racing import CarRacing
+
+
+class CarRacingPenaltyEnv(CarRacing):
+    """A version of CarRacing environment with penalty for going off track."""
+
+    def __init__(self, penalty_coeff: float = 1.0, **kwargs):
+        super().__init__(**kwargs)
+        self.penalty_coeff = penalty_coeff
+
+    def step(self, action):
+        obs, reward, done, truncated, info = super().step(action)
+        # Apply penalty for going off track
+        tile_contacts = [len(w.tiles) for w in self.car.wheels]
+        penalty = 4 - sum(tile_contacts)  # Number of wheels off track
+        reward -= self.penalty_coeff * penalty
+        self.reward -= self.penalty_coeff * penalty
+        return obs, reward, done, truncated, info
+
+
+gym.register(id="CarRacingPenalty-v0", entry_point=CarRacingPenaltyEnv)
